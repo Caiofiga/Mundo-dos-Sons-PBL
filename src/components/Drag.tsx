005a23b7552a395e4ca5ff2221e58610a3254654
Caio Figueiredo <contascontao@gmail.com>
2023-06-02 12:14:45 -0300

    Added timer function to 1st game. Yay :)

--- src/components/Drag.tsx
+++ src/components/Drag.tsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "./UserContext";
 import { get } from "http";
 import { addAnswersToDB } from "./firebase";
+import { Stopwatch } from "ts-stopwatch";
 
 const microfone = "/src/img/microfone.png";
 const palavras = ["jabuticaba", "tartaruga", "baleia", "barco", "pirata"];
@@ -15,6 +16,8 @@ const silabas = [
   ["ta", "bi", "pi", "ra", "la"],
 ];
 const resposta1: string[] = [];
+const Tempos: number[] = [];
+const stopwatch = new Stopwatch();
 
 const JaSom = "/src/snd/ja.mp3";
 const BuSom = "/src/snd/bu.mp3";
@@ -58,35 +61,59 @@ function generateInitialData(currentLine) {
 
 function Drag() {
   const [currentLine, setCurrentLine] = useState(0);
+  const [levelStarted, setLevelStarted] = useState(false);
+  const [gameOver, setGameOver] = useState(false);
   const [data, setData] = useState(generateInitialData(currentLine));
   const palavra = palavras[currentLine];
   const imagem = "/src/img/" + palavra + ".png";
   const navigate = useNavigate();
   const { userId } = React.useContext(UserContext);
+  const [timer, setTimer] = useState(0);
+
+  const timerRef = useRef(null);
 
   const getWordFromList = () => {
     return data.list2.map((item) => item.content).join("");
   };
 
+  const startLevel = () => {
+    setLevelStarted(true);
+    stopwatch.start();
+  };
+
+  const endLevel = () => {
+    setLevelStarted(false);
+    stopwatch.stop();
+    Tempos.push(stopwatch.getTime());
+    console.log(Tempos);
+    stopwatch.reset();
+    loadNextLine();
+  };
+  useEffect(() => {
+    setData(generateInitialData(currentLine));
+  }, [currentLine]);
+
+  useEffect(() => {
+    if (gameOver) {
+      endLevel();
+    }
+  }, [gameOver]);
+
   const loadNextLine = () => {
-    console.log(getWordFromList());
     if (currentLine < silabas.length - 1) {
       setCurrentLine(currentLine + 1);
-      setData(generateInitialData(currentLine + 1));
+      setLevelStarted(false);
       resposta1.push(getWordFromList());
-      console.log(resposta1);
     } else {
-      // navigate to the next component
       resposta1.push(getWordFromList());
-      // Generate answerObj here after all answers have been added to resposta1
       let answerObj = { userId: userId };
       resposta1.slice(0).forEach((answer, index) => {
         answerObj[`resposta${index + 1}`] = answer;
+        console.log(answerObj);
       });
 
-      addAnswersToDB("perguntas1", answerObj);
-      alert("Parabéns! Você completou o jogo!");
-      navigate("/Silaba"); // replace "/Silaba" with the path of the next component you want to navigate to
+      addAnswersToDB("perguntas1", { answerObj, Tempos });
+      navigate("/Silaba");
     }
   };
 
@@ -125,69 +152,80 @@ function Drag() {
 
   return (
     <div className="app-container">
-      <img src={imagem} alt={palavra} style={{ width: "200px" }} />
-      <DragDropContext onDragEnd={handleDragEnd}>
-        {Object.keys(data).map((list) => (
-          <Droppable droppableId={list} direction="horizontal" key={list}>
-            {(provided, snapshot) => (
-              <div
-                className="row-container"
-                ref={provided.innerRef}
-                {...provided.droppableProps}
-                style={{
-                  display: "flex",
-                  margin: "10px",
-                  padding: "10px",
-                  border: "1px solid black",
-                  backgroundColor: snapshot.isDraggingOver
-                    ? "#e0e0e0"
-                    : "white",
-                }}
-              >
-                {data[list].map((item, index) => {
-                  const handlePlaySound = () => {
-                    item.audioRef.current.play();
-                  };
+      {!levelStarted ? (
+        <button onClick={startLevel}>Start Level</button>
+      ) : gameOver ? (
+        <div>
+          <h2>Game Over!</h2>
+          <button onClick={() => navigate("/Silaba")}>Next Level</button>
+        </div>
+      ) : (
+        <>
+          <img src={imagem} alt={palavra} style={{ width: "200px" }} />
+          <DragDropContext onDragEnd={handleDragEnd}>
+            {Object.keys(data).map((list) => (
+              <Droppable droppableId={list} direction="horizontal" key={list}>
+                {(provided, snapshot) => (
+                  <div
+                    className="row-container"
+                    ref={provided.innerRef}
+                    {...provided.droppableProps}
+                    style={{
+                      display: "flex",
+                      margin: "10px",
+                      padding: "10px",
+                      border: "1px solid black",
+                      backgroundColor: snapshot.isDraggingOver
+                        ? "#e0e0e0"
+                        : "white",
+                    }}
+                  >
+                    {data[list].map((item, index) => {
+                      const handlePlaySound = () => {
+                        item.audioRef.current.play();
+                      };
 
-                  return (
-                    <Draggable
-                      draggableId={item.id}
-                      index={index}
-                      key={item.id}
-                    >
-                      {(provided) => (
-                        <div
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                          className="draggable-item"
+                      return (
+                        <Draggable
+                          draggableId={item.id}
+                          index={index}
+                          key={item.id}
                         >
-                          <img
-                            src={item.imgUrl}
-                            alt={item.content}
-                            style={{ width: "50px", height: "50px" }}
-                            onClick={handlePlaySound}
-                          />
-                          {item.content}
-                          <audio
-                            ref={item.audioRef}
-                            src={item.audioSrc}
-                            style={{ display: "none" }}
-                          />
-                        </div>
-                      )}
-                    </Draggable>
-                  );
-                })}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-        ))}
-      </DragDropContext>
-      <button onClick={loadNextLine}>
-        {currentLine < silabas.length - 1 ? "Próxima linha" : "Next Level"}
-      </button>
+                          {(provided) => (
+                            <div
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                              className="draggable-item"
+                            >
+                              <img
+                                src={item.imgUrl}
+                                alt={item.content}
+                                style={{ width: "50px", height: "50px" }}
+                                onClick={handlePlaySound}
+                              />
+                              {item.content}
+                              <audio
+                                ref={item.audioRef}
+                                src={item.audioSrc}
+                                style={{ display: "none" }}
+                              />
+                            </div>
+                          )}
+                        </Draggable>
+                      );
+                    })}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            ))}
+          </DragDropContext>
+          <button onClick={endLevel}>
+            {currentLine < silabas.length - 1 ? "Next Line" : "End Game"}
+          </button>
+        </>
+      )}
     </div>
   );
 }
