22102d14c3f33cb2de1da3fe728da369c45e31be
Caio Figueiredo <contascontao@gmail.com>
2023-11-04 11:13:17 -0300

    removed navbar from main

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./css/silaba.css"; // Assuming you have your styles in index.css
 import "./css/drag.css"; // Assuming you have your styles in index.css
-import Routers,  { Navbar } from "./components/Route";
-
+import Routers from "./components/Route";
 
 ReactDOM.render(
   <React.StrictMode>
