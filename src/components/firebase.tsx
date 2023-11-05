a5ed3a47f1a1f45d8217636391d03898f503965b
Caio Figueiredo <Contascontao@gmail.com>
2023-11-05 12:28:16 -0300

    opsei daisei

--- src/components/firebase.tsx
+++ src/components/firebase.tsx
@@ -11,12 +11,12 @@ import {
   doc,
   getDoc,
 } from "firebase/firestore";
-import firebaseConfig from "./firebaseconfig";
+import { firebaseConfig } from "./firebaseconfig";
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 
-export async function addUserToDB(user:string[]) {
+export async function addUserToDB(user: string[]) {
   try {
     await addDoc(collection(db, "users"), user);
   } catch (e) {
@@ -24,7 +24,10 @@ export async function addUserToDB(user:string[]) {
   }
 }
 
-export async function addAnswersToDB(database: string, answers: string[] | number[] | any) {
+export async function addAnswersToDB(
+  database: string,
+  answers: string[] | number[] | any
+) {
   try {
     await addDoc(collection(db, database), answers);
   } catch (e) {
@@ -37,7 +40,7 @@ export async function getDocsByUserId(collectionName: string, userId: string) {
   const q = query(collectionRef, where("userId", "==", userId));
   const querySnapshot = await getDocs(q);
 
-  const docs:any[] = [];
+  const docs: any[] = [];
   querySnapshot.forEach((doc) => {
     // doc.data() is never undefined for query doc snapshots
     console.log(doc.id, " => ", doc.data());
@@ -47,11 +50,11 @@ export async function getDocsByUserId(collectionName: string, userId: string) {
   return docs;
 }
 export async function getUser(nome: string, sobrenome: string) {
-  const userCollection = collection(db, "users")
+  const userCollection = collection(db, "users");
   const q = query(
     userCollection,
-    where("nome", "==", nome ),
-    where("sobrenome", "==", sobrenome),
+    where("nome", "==", nome),
+    where("sobrenome", "==", sobrenome)
   );
   console.log("q = ", q);
 
@@ -59,26 +62,24 @@ export async function getUser(nome: string, sobrenome: string) {
   console.log("query = ", querySnapshot);
 
   // Extract data from the snapshot
-  const users = querySnapshot.docs.map(doc => doc.data());
+  const users = querySnapshot.docs.map((doc) => doc.data());
   console.log("users =", users);
 
   return users;
 }
-export async function getAllUsers(){
-  const q =query(
-    collection(db, "users")
-  )
-  const querySnapshot = await getDocs(q)
-  const users = querySnapshot.docs.map(doc => doc.data())
+export async function getAllUsers() {
+  const q = query(collection(db, "users"));
+  const querySnapshot = await getDocs(q);
+  const users = querySnapshot.docs.map((doc) => doc.data());
   return users;
 }
 
-export async function DeleteData(nome: string, sobrenome: string, ) {
+export async function DeleteData(nome: string, sobrenome: string) {
   const userCollection = collection(db, "users");
   const q = query(
     userCollection,
     where("nome", "==", nome),
-    where("sobrenome", "==", sobrenome),
+    where("sobrenome", "==", sobrenome)
   );
 
   const querySnapshot = await getDocs(q);
@@ -87,51 +88,62 @@ export async function DeleteData(nome: string, sobrenome: string, ) {
   if (!querySnapshot.empty) {
     const userDoc = querySnapshot.docs[0]; // Get the first matching document
     const userRef = doc(db, "users", userDoc.id); // Get a reference to the document.
-    await updateDoc(userRef, { nome: 'NewName', sobrenome: 'NewSurname', age: 25 });
+    await updateDoc(userRef, {
+      nome: "NewName",
+      sobrenome: "NewSurname",
+      age: 25,
+    });
   } else {
-    console.log('No user found with the provided nome and sobrenome');
+    console.log("No user found with the provided nome and sobrenome");
   }
 }
 
-export async function GetUserId(nome: string, sobrenome: string, idade: string){
+export async function GetUserId(
+  nome: string,
+  sobrenome: string,
+  idade: string
+) {
   const userCollection = collection(db, "users");
   const q = query(
     userCollection,
     where("nome", "==", nome),
     where("sobrenome", "==", sobrenome),
-    where("idade", "==", idade),
+    where("idade", "==", idade)
   );
   const querySnapshot = await getDocs(q);
-  const users = querySnapshot.docs.map(doc => ({ id: doc.id }));
-  console.log(users)
+  const users = querySnapshot.docs.map((doc) => ({ id: doc.id }));
+  console.log(users);
   return users.length > 0 ? users[0].id : null;
 }
 
-export async function DelDocByID( documentId: string) {
+export async function DelDocByID(documentId: string) {
   const docRef = doc(db, "users", documentId);
   const docSnap = await getDoc(docRef);
 
   if (docSnap.exists()) {
-    await updateDoc(docRef, { nome: 'Pessoa', sobrenome: 'Anonima', age: 25 });
+    await updateDoc(docRef, { nome: "Pessoa", sobrenome: "Anonima", age: 25 });
     return docSnap.data();
   } else {
     console.log("No such document!");
   }
 }
-export async function GetCustomUId(nome:string, sobrenome: string, idade: string) {
-  const UCollection = collection(db, "users")
+export async function GetCustomUId(
+  nome: string,
+  sobrenome: string,
+  idade: string
+) {
+  const UCollection = collection(db, "users");
   const q = query(
-    UCollection, 
+    UCollection,
     where("nome", "==", nome),
     where("sobrenome", "==", sobrenome),
-    where("idade", "==", idade),
-  )
-const docSnap = await getDocs(q);
-if(!docSnap.empty){
-  const UserId = docSnap.docs[0].data().userId;
-  return UserId;
-}
-
+    where("idade", "==", idade)
+  );
+  const docSnap = await getDocs(q);
+  if (!docSnap.empty) {
+    const UserId = docSnap.docs[0].data().userId;
+    return UserId;
+  }
 }
 
 // Add other Firestore functions as needed
