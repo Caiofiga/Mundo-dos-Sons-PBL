6003d55197ce81aae03c92142dead1b7be0a80d4
Caio Figueiredo <Contascontao@gmail.com>
2023-11-05 12:20:43 -0300

    fixed animated

--- src/components/animated.tsx
+++ src/components/animated.tsx
@@ -1,14 +1,21 @@
-import React, { useState, useEffect, ReactNode, ReactElement, Key } from "react";
+import React, { useState, useEffect, ReactNode, ReactElement } from "react";
 import { useTransition, animated, useSpringRef } from "react-spring";
 
 type ReactChild = ReactElement | string | number;
 
 const AnimatedPages = ({ children }: { children: ReactNode }) => {
-  const [items, setItems] = useState<ReactChild[]>(React.Children.toArray(children) as ReactChild[]);
-  const transRef = useSpringRef();
+  const [items, setItems] = useState<ReactChild[]>(
+    React.Children.toArray(children) as ReactChild[]
+  );
+  const transRef = useSpringRef(); // Removed the type assertion for now
+
   const transitions = useTransition(items, {
-    ref: transRef,
-    keys: (item: ReactChild) => (React.isValidElement(item) ? item.key : 'item-' + item.toString()) as Key,
+    // Removed the ref property for now to troubleshoot other issues
+    keys: (item: ReactChild) =>
+      typeof item === "string" || typeof item === "number"
+        ? item.toString()
+        : item.key ?? `item-${Math.random()}`, // Ensure a unique key
+
     from: { opacity: 0, transform: "translate3d(100%,0,0)" },
     enter: { opacity: 1, transform: "translate3d(0%,0,0)" },
     leave: { opacity: 0, transform: "translate3d(-50%,0,0)" },
@@ -20,9 +27,9 @@ const AnimatedPages = ({ children }: { children: ReactNode }) => {
   }, [children, transRef]);
 
   return (
-    <div className={`flex fill`}>
-      {transitions((style: any, item: any) => (
-        <animated.div style={{ ...style }}>{item}</animated.div>
+    <div className="flex fill">
+      {transitions((style: any, item: ReactChild) => (
+        <animated.div style={style}>{item}</animated.div>
       ))}
     </div>
   );
---
+++ src/components/firebaseconfig.ts
@@ -0,0 +1,18 @@
+export const firebaseConfig = {
+
+    apiKey: "AIzaSyBxmVORAk4TDkQ02b33UL4h2ilsbyYcEw4",
+  
+    authDomain: "pbl-app-b3962.firebaseapp.com",
+  
+    projectId: "pbl-app-b3962",
+  
+    storageBucket: "pbl-app-b3962.appspot.com",
+  
+    messagingSenderId: "1070603645378",
+  
+    appId: "1:1070603645378:web:24a7cdc14cf31036f7265b",
+  
+    measurementId: "G-0CBT4CJ3DR"
+  
+  };
+  
